cc7b121acc1e3e2caca50091e42b6518918f1be4
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-21 17:32:28 -0600

    Created variables to store the response from axios

--- index.ts
+++ index.ts
@@ -3,5 +3,10 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 axios.get(url).then(response => {
-  console.log("it worked")
+  const todo = response.data
+  console.log(todo)
+
+  const ID = todo.id
+  const title = todo.title
+  console.log(`id of ${ID} and title ${title}`)
 })
