0fcd102168c4422b7a005aaddea6cdf7c71c167a
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-21 17:49:20 -0600

    Created interface for data and set it

--- index.ts
+++ index.ts
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  const todo = response.data
-  console.log(todo)
+  const todo = response.data as Todo;
 
   const ID = todo.id
   const title = todo.title
