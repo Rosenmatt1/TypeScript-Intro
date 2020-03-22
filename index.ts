527c000cd039b72d3976384cb3f0a50a4e75768a
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-21 18:18:56 -0600

    Added typing to function

--- index.ts
+++ index.ts
@@ -11,7 +11,14 @@ interface Todo {
 axios.get(url).then(response => {
   const todo = response.data as Todo;
 
-  const ID = todo.id
+  const id = todo.id
   const title = todo.title
-  console.log(`id of ${ID} and title ${title}`)
+  const completed = todo.completed
+  console.log(`id of ${id} and title ${title} is completed: ${completed}`)
+
+  logTodo(id, title, completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`id of ${id} and title ${title} is completed: ${completed}`)
+}
